edaf67e2c941145b8f8b80ea69a4ae8e476fe2df
Diego Jara <saintplay96@gmail.com>
2017-10-08 21:51:02 -0500

    Agrega informacion extra en el footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,20 @@
     <transition name="fade" mode="out-in">
       <router-view/>
     </transition>
+    <div class="columns">
+      <div class="column is-6 is-offset-3">
+        <article class="message is-info">
+          <div class="message-body">
+            <p>
+              Este aplicativo aún se encuentra en etapas de desarrollo! Si tienes sugerencias o dudas porfavor escríbeme a <em>saintplay96@gmail.com</em>
+            </p>
+            <strong>
+              El código de esta aplicación es completamente <em>Open Source</em>, el código lo puedes encontrar <a class="is-link" href="https://github.com/saintplay/octopus-paul">Github</a>
+            </strong>
+          </div>
+        </article>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,6 +36,10 @@ $scu-margins: (0 5 10 15 20 25 30);
 
 @import "assets/sass/colors";
 @import "assets/sass/animations/all";
+
+html, body {
+  height: 100%;
+}
 
 body {
   background-color: $transparent-gray;
